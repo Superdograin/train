71af49d4877cff058844c5949ab2aa8db2b73c4d
xnzivu <xnzivu@126.com>
2024-05-21 23:49:27 +0800

    create module "member"

--- member/src/main/java/cn/qiluno/train/member/controller/TestController.java
+++ member/src/main/java/cn/qiluno/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package cn.qiluno.train;
+package cn.qiluno.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
