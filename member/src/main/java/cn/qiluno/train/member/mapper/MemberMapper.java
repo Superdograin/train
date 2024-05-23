ca66db91943c200728e576d3d100a9744fa037f5
xnzivu <xnzivu@126.com>
2024-05-23 16:11:55 +0800

    集成 MyBatis Generator

--- member/src/main/java/cn/qiluno/train/member/mapper/MemberMapper.java
+++ member/src/main/java/cn/qiluno/train/member/mapper/MemberMapper.java
@@ -1,5 +1,31 @@
 package cn.qiluno.train.member.mapper;
 
+import cn.qiluno.train.member.domain.Member;
+import cn.qiluno.train.member.domain.MemberExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface MemberMapper {
-    int count();
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member row);
+
+    int insertSelective(Member row);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Member row, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("row") Member row, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member row);
+
+    int updateByPrimaryKey(Member row);
 }
