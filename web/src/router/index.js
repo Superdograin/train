017e99f3e105d70e2a8788ce0aa86934a9d3970d
xnzivu <xnzivu@126.com>
2024-05-28 19:22:23 +0800

    路由登录拦截

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { notification } from "ant-design-vue";
+import store from "@/store";
 
 const routes = [
   {
@@ -8,6 +10,9 @@ const routes = [
   {
     path: '/',
     component: () => import('../views/main.vue'),
+    meta: {
+      loginRequired : true
+    },
     children: [
       {
         path: 'welcome',
@@ -24,6 +29,26 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验: ", item.meta.loginRequired || false)
+    return item.meta.loginRequired
+  })) {
+    const member = store.state.member
+    console.log("页面登录校验开始： ", member)
+    if (!member.token) {
+      console.log("用户未登录或登录超时！")
+      notification.error({ description: "未登录或登录超时" })
+      next('login')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
