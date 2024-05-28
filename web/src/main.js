e813cf652d159b8b46d2bcc64d4b54e27f658a9d
xnzivu <xnzivu@126.com>
2024-05-28 15:59:56 +0800

    axios 拦截器

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue'
+import Antd, { notification } from 'ant-design-vue'
 import "ant-design-vue/dist/antd.css"
 import * as Icons from '@ant-design/icons-vue'
 import axios from 'axios'
@@ -20,6 +20,11 @@ for (const i in icons) {
 axios.interceptors.request.use(
     function(config) {
         console.log('请求参数： ', config)
+        const token = store.state.member.token
+        if (token) {
+            config.headers.token = token
+            console.log("请求 headers 带上 token: ", token)
+        }
         return config
     },
     function (error) {
@@ -33,6 +38,15 @@ axios.interceptors.response.use(
     },
     function (error) {
         console.log('返回错误： ', error)
+        const response = error.response
+        const status = response.status
+        if (status === 401) {
+            // 判断状态码是401 跳转到登录页
+            console.log("未登录或登录超时，跳到登录页");
+            store.commit("setMember", {});
+            notification.error({ description: "未登录或登录超时" });
+            router.push('/login').then(() => {});
+        }
         return Promise.reject(error);
     }
 )
