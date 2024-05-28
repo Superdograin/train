e0d05d714926453954dd9b7d58d79a074150a90e
xnzivu <xnzivu@126.com>
2024-05-28 20:06:30 +0800

    新建乘车人表和乘客类型枚举类

--- sql/member.sql
+++ sql/member.sql
@@ -5,3 +5,16 @@ CREATE TABLE `member`  (
   PRIMARY KEY (`id`),
   UNIQUE KEY `mobile_unique` (`mobile`)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT = '会员表';
+
+DROP TABLE IF EXISTS `passenger`;
+CREATE TABLE `train_member`.`passenger`  (
+  `id` bigint NOT NULL COMMENT 'id',
+  `member_id` bigint NOT NULL COMMENT '会员id',
+  `name` varchar(20) NOT NULL COMMENT '姓名',
+  `id_card` varchar(18) NOT NULL COMMENT '身份证',
+  `type` char(1) NOT NULL COMMENT '旅客类型 | 枚举[PassengerTypeEnum]',
+  `create_time` datetime(3) NULL COMMENT '新增时间',
+  `update_time` datetime(3) NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  INDEX `member_id_index`(`member_id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT = '乘车人';
